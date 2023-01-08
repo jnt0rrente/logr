1613ad6c264310444e521862097d3f27860cec4a
Juan Torrente <jnatorrente@gmail.com>
2023-01-08 13:14:12 +0100

    geolocation router and controller

--- api/geolocationlog/geolocationLogRouter.js
+++ api/geolocationlog/geolocationLogRouter.js
@@ -2,12 +2,10 @@ const {
     body
 } = require("express-validator")
 const express = require("express")
-const rawLogController = require("./rawLogController")
+const rawLogController = require("./geolocationLogController")
 const router = express.Router()
 
 router.post("/log",
-    body("coordinates").notEmpty().isLatLong().trim().escape(),
-    body("id").notEmpty().trim().escape(),
     body("content").notEmpty().trim().escape(),
     rawLogController.saveLog
 )
