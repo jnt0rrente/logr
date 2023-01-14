2c11e7b9f7801850a749b938ea0d1cbfa129d273
Juan Torrente <jnatorrente@gmail.com>
2023-01-14 22:07:14 +0100

    auth linked

--- api/admin/adminRouter.js
+++ api/admin/adminRouter.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
+const authorizationRouter = require("./authorization/authorizationRouter")
+router.use("/auth", authorizationRouter)
+
 const registrationRouter = require("./registration/registrationRouter")
 router.use("/register", registrationRouter)
 
